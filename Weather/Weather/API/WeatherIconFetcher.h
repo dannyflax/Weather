474cdee727cec2242beb129f3ec1ee5bc257df03
Danny Flax <dannyflax@fb.com>
2023-08-25 16:47:35 -0400

    add comment about image fetching

--- Weather/Weather/API/WeatherIconFetcher.h
+++ Weather/Weather/API/WeatherIconFetcher.h
@@ -11,6 +11,12 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/*
+ * Fetches icons from the weather service.
+ * To include image caching, I would consider first in-memory caching.
+ * I would make the fetcher into a concrete class instead of class methods, then store a lookup dictionary with previously fetched images.
+ * Then disk cache could then follow.
+ */
 @interface WeatherIconFetcher : NSObject
 
 + (void)fetchIconWithName:(NSString *)iconName completionBlock:(void(^)(UIImage *))completionBlock;
